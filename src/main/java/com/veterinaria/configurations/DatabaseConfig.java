418b4cb0c8b7e06245b31dfb53b46337d02277cd
Daniel Lorenzo <daniellorenzo47@aragon.unam.mx>
2023-08-27 23:34:52 -0600

    Try database config to password into enviroment

---
+++ src/main/java/com/veterinaria/configurations/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.veterinaria.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConfig {
+
+    @Autowired
+    private Environment env;
+
+    public String getDatabasePassword() {
+        return env.getProperty("SPRING_DATASOURCE_PASSWORD");
+    }
+
+
+}
